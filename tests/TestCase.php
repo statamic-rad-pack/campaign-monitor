c07d5021111b94aa49c24d82468a11f27ebfd222
Ryan Mitchell <ryan@thoughtcollective.com>
2024-04-12 09:34:14 +0100

    Switch to using Statamic 5's add on test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,73 +2,19 @@
 
 namespace StatamicRadPack\CampaignMonitor\Tests;
 
-use JMac\Testing\Traits\AdditionalAssertions;
-use Orchestra\Testbench\TestCase as OrchestraTestCase;
-use Statamic\Extend\Manifest;
+use Statamic\Extend\AddonTestCase;
 use Statamic\Facades\Blueprint as BlueprintFacade;
 use Statamic\Facades\YAML;
-use Statamic\Providers\StatamicServiceProvider;
 use Statamic\Statamic;
 use StatamicRadPack\CampaignMonitor\ServiceProvider;
 
-class TestCase extends OrchestraTestCase
+class TestCase extends AddonTestCase
 {
-    use AdditionalAssertions, PreventSavingStacheItemsToDisk;
-
-    public function setup(): void
-    {
-        parent::setup();
-        $this->preventSavingStacheItemsToDisk();
-    }
-
-    public function tearDown(): void
-    {
-        $this->deleteFakeStacheDirectory();
-
-        parent::tearDown();
-    }
-
-    protected function getPackageProviders($app)
-    {
-        return [StatamicServiceProvider::class, ServiceProvider::class];
-    }
-
-    protected function getPackageAliases($app)
-    {
-        return [
-            'Statamic' => Statamic::class,
-        ];
-    }
-
-    protected function getEnvironmentSetUp($app)
-    {
-        parent::getEnvironmentSetUp($app);
-
-        $app->make(Manifest::class)->manifest = [
-            'statamic-rad-pack/campaign-monitor' => [
-                'id' => 'statamic-rad-pack/campaign-monitor',
-                'namespace' => 'StatamicRadPack\\CampaignMonitor',
-            ],
-        ];
-
-        config(['statamic.users.repository' => 'file']);
-    }
+    protected string $addonServiceProvider = ServiceProvider::class;
 
     protected function resolveApplicationConfiguration($app)
     {
         parent::resolveApplicationConfiguration($app);
-
-        $configs = ['assets', 'cp', 'forms', 'routes', 'static_caching', 'sites', 'stache', 'system', 'users'];
-
-        foreach ($configs as $config) {
-            $app['config']->set("statamic.$config", require __DIR__."/../vendor/statamic/cms/config/{$config}.php");
-        }
-
-        // Setting the user repository to the default flat file system
-        $app['config']->set('statamic.users.repository', 'file');
-
-        // Assume the pro edition within tests
-        $app['config']->set('statamic.editions.pro', true);
 
         Statamic::booted(function () {
             $blueprintContents = YAML::parse(file_get_contents(__DIR__.'/__fixtures__/blueprints/contact_us.yaml'));
